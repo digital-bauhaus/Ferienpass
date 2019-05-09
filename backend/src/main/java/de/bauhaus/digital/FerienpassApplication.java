31ac2cfd6f270df04f0db8ffcb2faaf3a1d1fabc
Jonas Hecht <jonas.hecht@codecentric.de>
2019-05-09 11:23:05 +0200

    #46 eliminating deprecation warnings at startup

--- backend/src/main/java/de/bauhaus/digital/FerienpassApplication.java
+++ backend/src/main/java/de/bauhaus/digital/FerienpassApplication.java
@@ -7,10 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDate;
 
