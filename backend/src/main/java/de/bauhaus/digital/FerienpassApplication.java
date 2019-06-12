7b7df1eab9a868191966f862ff7701518548028e
Jonas Hecht <jonas.hecht@codecentric.de>
2019-06-12 14:09:00 +0200

    #57 Check for existance of environment variable SENDGRID_API_KEY and mail/mailtext.txt, otherwise shutdown Spring Boot application context with error log

--- backend/src/main/java/de/bauhaus/digital/FerienpassApplication.java
+++ backend/src/main/java/de/bauhaus/digital/FerienpassApplication.java
@@ -1,12 +1,30 @@
 package de.bauhaus.digital;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FerienpassApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(FerienpassApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(FerienpassApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(FerienpassApplication.class, args);
+
+		checkIfMailtextIsPresentAndShutdownSpringBootIfNot(ctx);
+	}
+
+	private static void checkIfMailtextIsPresentAndShutdownSpringBootIfNot(ConfigurableApplicationContext ctx) {
+		try {
+			ctx.getBean("backendController");
+			LOG.info("Precondition-Check bestanden: Env-Variable SENDGRID_API_KEY und Mail-Konfigurationsdatei gefunden!");
+		} catch (NoSuchBeanDefinitionException noSuchBean) {
+			LOG.error("Precondition-Check NICHT bestanden: Entweder es wurde die Env-Variable SENDGRID_API_KEY nicht gesetzt oder die Mail-Konfigurationsdatei backend/src/main/resources/mail/mailtext.txt nicht gefunden: " + noSuchBean.getMessage());
+			ctx.close();
+		}
 	}
 }
--- backend/src/main/java/de/bauhaus/digital/controller/BackendController.java
+++ backend/src/main/java/de/bauhaus/digital/controller/BackendController.java
@@ -21,6 +21,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +38,10 @@ import java.util.Optional;
 
 @RestController()
 @RequestMapping("/api")
+@ConditionalOnResource(resources = "mail/mailtext.txt") // check, whether mail/mailtext.txt is on the classpath, since we need it for sending mails!
+@ConditionalOnProperty(
+        value = "SENDGRID_API_KEY" // check, whether SENDGRID_API_KEY environment variable is set, since we need it for sending mails!
+)
 public class BackendController {
 
     private static final Logger LOG = LoggerFactory.getLogger(BackendController.class);
