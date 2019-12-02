a3e5dd444619da3de525de50e4d9e02f5adef5b9
Bertram Vogel <bertram.vogel@codecentric.de>
2019-12-02 15:26:52 +0100

    Made router and store compatible with the vue cli v4 conventions.

---
+++ frontend/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import { state, getters } from "./state";
+import actions from "./actions";
+import mutations from "./mutations";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+});
