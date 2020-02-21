6a3b2b0bf218c9efa48da514aaf35f6a74f7d515
Bertram Vogel <bertram.vogel@codecentric.de>
2020-02-21 03:09:37 +0100

    #95: improved layout

--- frontend/src/components/VeranstaltungEditor.vue
+++ frontend/src/components/VeranstaltungEditor.vue
@@ -9,101 +9,114 @@
       :validated="showValidationStatus"
       @submit="onSubmit"
     >
-      <b-form-group
-        id="veranstaltung-name-group"
-        label-for="veranstaltung-name-value"
-        label="Name"
-      >
-        <b-form-input
-          id="veranstaltung-name-value"
-          v-model.trim="veranstaltung.name"
-          required
-          placeholder="Name der Veranstaltung"
-        />
-      </b-form-group>
+      <b-form-row>
+        <b-col sm="12">
+          <b-form-group
+            id="veranstaltung-name-group"
+            label-for="veranstaltung-name-value"
+            label="Name"
+          >
+            <b-form-input
+              id="veranstaltung-name-value"
+              v-model.trim="veranstaltung.name"
+              required
+              placeholder="Name der Veranstaltung"
+            />
+          </b-form-group>
+        </b-col>
 
-      <b-form-group
-        id="veranstaltung-begindate-group"
-        label-for="veranstaltung-begindate-value"
-        label="Beginndatum"
-      >
-        <b-form-input
-          id="veranstaltung-begindate-value"
-          v-model.trim="veranstaltung.datum"
-          required
-          placeholder="Datum (TT.MM.JJJ)"
-          type="date"
-        />
-      </b-form-group>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-begindate-group"
+            label-for="veranstaltung-begindate-value"
+            label="Beginndatum"
+          >
+            <b-form-input
+              id="veranstaltung-begindate-value"
+              v-model.trim="veranstaltung.datum"
+              required
+              placeholder="Datum (TT.MM.JJJ)"
+              type="date"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-enddate-group"
+            label-for="veranstaltung-enddate-value"
+            label="Enddatum"
+          >
+            <b-form-input
+              id="veranstaltung-enddate-value"
+              v-model.trim="veranstaltung.datumEnde"
+              required
+              placeholder="Datum (TT.MM.JJJ)"
+              type="date"
+            />
+          </b-form-group>
+        </b-col>
 
-      <b-form-group
-        id="veranstaltung-enddate-group"
-        label-for="veranstaltung-enddate-value"
-        label="Enddatum"
-      >
-        <b-form-input
-          id="veranstaltung-enddate-value"
-          v-model.trim="veranstaltung.datumEnde"
-          required
-          placeholder="Datum (TT.MM.JJJ)"
-          type="date"
-        />
-      </b-form-group>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-slotstotal-group"
+            label-for="veranstaltung-slotstotal-value"
+            label="Plätze (gesamt)"
+          >
+            <b-form-input
+              id="veranstaltung-slotstotal-value"
+              v-model.trim="veranstaltung.slotsGesamt"
+              required
+              placeholder="Plätze"
+              type="number"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-slotsreserved-group"
+            label-for="veranstaltung-slotsreserved-value"
+            label="Plätze (reserviert)"
+          >
+            <b-form-input
+              id="veranstaltung-slotsreserved-value"
+              v-model.trim="veranstaltung.slotsReserviert"
+              placeholder="Reservierte Plätze"
+              type="number"
+            />
+          </b-form-group>
+        </b-col>
 
-      <b-form-group
-        id="veranstaltung-slotstotal-group"
-        label-for="veranstaltung-slotstotal-value"
-        label="Plätze (gesamt)"
-      >
-        <b-form-input
-          id="veranstaltung-slotstotal-value"
-          v-model.trim="veranstaltung.slotsGesamt"
-          required
-          placeholder="Plätze"
-          type="number"
-        />
-      </b-form-group>
-
-      <b-form-group
-        id="veranstaltung-slotsreserved-group"
-        label-for="veranstaltung-slotsreserved-value"
-        label="Plätze (reserviert)"
-      >
-        <b-form-input
-          id="veranstaltung-slotsreserved-value"
-          v-model.trim="veranstaltung.slotsReserviert"
-          placeholder="Reservierte Plätze"
-          type="number"
-        />
-      </b-form-group>
-
-      <b-form-group
-        id="veranstaltung-minage-group"
-        label-for="veranstaltung-minage-value"
-        label="Mindestalter"
-      >
-        <b-form-input
-          id="veranstaltung-minage-value"
-          v-model.trim="veranstaltung.mindestAlter"
-          required
-          placeholder="Altersbegrenzung"
-          type="number"
-        />
-      </b-form-group>
-
-      <b-form-group
-        id="veranstaltung-maxage-group"
-        label-for="veranstaltung-maxage-value"
-        label="Höchstalter"
-      >
-        <b-form-input
-          id="veranstaltung-maxage-value"
-          v-model.trim="veranstaltung.hoechstAlter"
-          required
-          placeholder="Altersbegrenzung"
-          type="number"
-        />
-      </b-form-group>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-minage-group"
+            label-for="veranstaltung-minage-value"
+            label="Mindestalter"
+          >
+            <b-form-input
+              id="veranstaltung-minage-value"
+              v-model.trim="veranstaltung.mindestAlter"
+              required
+              placeholder="Altersbegrenzung"
+              type="number"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col sm="6">
+          <b-form-group
+            id="veranstaltung-maxage-group"
+            label-for="veranstaltung-maxage-value"
+            label="Höchstalter"
+          >
+            <b-form-input
+              id="veranstaltung-maxage-value"
+              v-model.trim="veranstaltung.hoechstAlter"
+              required
+              placeholder="Altersbegrenzung"
+              type="number"
+            />
+          </b-form-group>
+        </b-col>
+      </b-form-row>
 
       <b-button
         type="submit"
@@ -123,7 +136,6 @@
     >
       {{ successText }}
     </b-alert>
-
   </div>
 </template>
 
