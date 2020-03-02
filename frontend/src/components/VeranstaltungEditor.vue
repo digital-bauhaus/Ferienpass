dff5e8fe9f74d2cfac09dbf16dde43d67ea89bc2
Bertram Vogel <bertram.vogel@codecentric.de>
2020-03-02 01:01:48 +0100

    #91: Moved data-fetching logic to the view where it belongs. VeranstaltungEditor-Component uses a v-model for the given veranstaltung now.

--- frontend/src/components/VeranstaltungEditor.vue
+++ frontend/src/components/VeranstaltungEditor.vue
@@ -1,241 +1,172 @@
 <template>
-  <div>
-    <h1>
-      {{ titleText }}
-    </h1>
+  <b-form
+    ref="form"
+    novalidate
+    :validated="showValidationStatus"
+    @submit="onSubmit"
+  >
+    <b-form-row>
+      <b-col sm="12">
+        <b-form-group
+          id="veranstaltung-name-group"
+          label-for="veranstaltung-name-value"
+          label="Name"
+        >
+          <b-form-input
+            id="veranstaltung-name-value"
+            required
+            trim
+            placeholder="Name der Veranstaltung"
+            :value="value.name"
+            @update="updateValue('name', $event)"
+          />
+        </b-form-group>
+      </b-col>
 
-    <ErrorAlert
-      v-if="showErrorAlert"
-      :heading-text=errorHeadingText
-      :errors="errorMessages"
-    />
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-begindate-group"
+          label-for="veranstaltung-begindate-value"
+          label="Beginndatum"
+        >
+          <b-form-input
+            id="veranstaltung-begindate-value"
+            type="date"
+            required
+            placeholder="Datum (TT.MM.JJJ)"
+            :value="value.datum"
+            @update="updateValue('datum', $event)"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-enddate-group"
+          label-for="veranstaltung-enddate-value"
+          label="Enddatum"
+        >
+          <b-form-input
+            id="veranstaltung-enddate-value"
+            type="date"
+            placeholder="Datum (TT.MM.JJJ)"
+            required
+            :value="value.datumEnde"
+            @update="updateValue('datumEnde', $event)"
+          />
+        </b-form-group>
+      </b-col>
 
-    <b-form
-      ref="form"
-      novalidate
-      :validated="showValidationStatus"
-      @submit="onSubmit"
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-slotstotal-group"
+          label-for="veranstaltung-slotstotal-value"
+          label="Plätze (gesamt)"
+        >
+          <b-form-input
+            id="veranstaltung-slotstotal-value"
+            type="number"
+            required
+            trim
+            placeholder="Plätze"
+            :value="value.slotsGesamt"
+            @update="updateValue('slotsGesamt', $event)"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-slotsreserved-group"
+          label-for="veranstaltung-slotsreserved-value"
+          label="Plätze (reserviert)"
+        >
+          <b-form-input
+            id="veranstaltung-slotsreserved-value"
+            type="number"
+            trim
+            placeholder="Reservierte Plätze"
+            :value="value.slotsReserviert"
+            @update="updateValue('slotsReserviert', $event)"
+          />
+        </b-form-group>
+      </b-col>
+
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-minage-group"
+          label-for="veranstaltung-minage-value"
+          label="Mindestalter"
+        >
+          <b-form-input
+            id="veranstaltung-minage-value"
+            type="number"
+            required
+            trim
+            placeholder="Altersbegrenzung"
+            :value="value.mindestAlter"
+            @update="updateValue('mindestAlter', $event)"
+          />
+        </b-form-group>
+      </b-col>
+      <b-col sm="6">
+        <b-form-group
+          id="veranstaltung-maxage-group"
+          label-for="veranstaltung-maxage-value"
+          label="Höchstalter"
+        >
+          <b-form-input
+            id="veranstaltung-maxage-value"
+            type="number"
+            required
+            trim
+            placeholder="Altersbegrenzung"
+            :value="value.hoechstAlter"
+            @update="updateValue('hoechstAlter', $event)"
+          />
+        </b-form-group>
+      </b-col>
+    </b-form-row>
+
+    <b-button
+      type="submit"
+      variant="primary"
     >
-      <b-form-row>
-        <b-col sm="12">
-          <b-form-group
-            id="veranstaltung-name-group"
-            label-for="veranstaltung-name-value"
-            label="Name"
-          >
-            <b-form-input
-              id="veranstaltung-name-value"
-              v-model.trim="veranstaltung.name"
-              required
-              placeholder="Name der Veranstaltung"
-            />
-          </b-form-group>
-        </b-col>
-
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-begindate-group"
-            label-for="veranstaltung-begindate-value"
-            label="Beginndatum"
-          >
-            <b-form-input
-              id="veranstaltung-begindate-value"
-              v-model.trim="veranstaltung.datum"
-              required
-              placeholder="Datum (TT.MM.JJJ)"
-              type="date"
-            />
-          </b-form-group>
-        </b-col>
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-enddate-group"
-            label-for="veranstaltung-enddate-value"
-            label="Enddatum"
-          >
-            <b-form-input
-              id="veranstaltung-enddate-value"
-              v-model.trim="veranstaltung.datumEnde"
-              required
-              placeholder="Datum (TT.MM.JJJ)"
-              type="date"
-            />
-          </b-form-group>
-        </b-col>
-
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-slotstotal-group"
-            label-for="veranstaltung-slotstotal-value"
-            label="Plätze (gesamt)"
-          >
-            <b-form-input
-              id="veranstaltung-slotstotal-value"
-              v-model.trim="veranstaltung.slotsGesamt"
-              required
-              placeholder="Plätze"
-              type="number"
-            />
-          </b-form-group>
-        </b-col>
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-slotsreserved-group"
-            label-for="veranstaltung-slotsreserved-value"
-            label="Plätze (reserviert)"
-          >
-            <b-form-input
-              id="veranstaltung-slotsreserved-value"
-              v-model.trim="veranstaltung.slotsReserviert"
-              placeholder="Reservierte Plätze"
-              type="number"
-            />
-          </b-form-group>
-        </b-col>
-
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-minage-group"
-            label-for="veranstaltung-minage-value"
-            label="Mindestalter"
-          >
-            <b-form-input
-              id="veranstaltung-minage-value"
-              v-model.trim="veranstaltung.mindestAlter"
-              required
-              placeholder="Altersbegrenzung"
-              type="number"
-            />
-          </b-form-group>
-        </b-col>
-        <b-col sm="6">
-          <b-form-group
-            id="veranstaltung-maxage-group"
-            label-for="veranstaltung-maxage-value"
-            label="Höchstalter"
-          >
-            <b-form-input
-              id="veranstaltung-maxage-value"
-              v-model.trim="veranstaltung.hoechstAlter"
-              required
-              placeholder="Altersbegrenzung"
-              type="number"
-            />
-          </b-form-group>
-        </b-col>
-      </b-form-row>
-
-      <b-button
-        type="submit"
-        variant="primary"
-      >
-        {{ submitButtonText }}
-      </b-button>
-    </b-form>
-
-    <b-alert
-      class="fixed-bottom w-50 mx-auto"
-      :show="dismissCountDown"
-      dismissible
-      variant="success"
-      @dismissed="dismissCountDown=0"
-      @dismiss-count-down="dismissCountDown = $event"
-    >
-      {{ successText }}
-    </b-alert>
-  </div>
+      {{ submitButtonText }}
+    </b-button>
+  </b-form>
 </template>
 
 <script>
-import api from '@/modules/ferienpass-api';
-import ErrorAlert from '@/components/ErrorAlert.vue';
 
 export default {
   name: 'Veranstaltung',
-  components: { ErrorAlert },
   props: {
-    veranstaltungId: {
-      type: Number,
+    value: {
+      type: Object,
+      required: true,
+    },
+    submitButtonText: {
+      type: String,
       required: true,
     },
   },
   data() {
     return {
-      veranstaltung: {},
       showValidationStatus: false,
-      errorMessages: [],
-      dismissCountDown: 0,
     };
   },
-  computed: {
-    showErrorAlert() {
-      return this.errorMessages.length > 0;
-    },
-    isNewProject() {
-      return this.veranstaltungId <= 0;
-    },
-    titleText() {
-      if (this.isNewProject) {
-        return 'Veranstaltung anlegen';
-      }
-      return 'Veranstaltungsbearbeitung';
-    },
-    submitButtonText() {
-      if (this.isNewProject) {
-        return 'Anlegen';
-      }
-      return 'Speichern';
-    },
-    successText() {
-      if (this.isNewProject) {
-        return 'Veranstaltung erfolgreich angelegt.';
-      }
-      return 'Veranstaltung erfolgreich gespeichert.';
-    },
-    errorHeadingText() {
-      if (this.isNewProject) {
-        return 'Anlegen nicht möglich. Bitte beheben Sie folgende Fehler:';
-      }
-      return 'Speichern nicht möglich. Bitte beheben Sie folgende Fehler:';
-    },
-  },
-  created() {
-    if (this.isNewProject) {
-      // TODO check why?
-      this.veranstaltung = {
-        aktiv: true,
-        anmeldungen: [],
-        stornierteTeilnehmer: [],
-      };
-    } else {
-      this.loadProjectData();
-    }
-  },
   methods: {
+    updateValue(propName, newValue) {
+      this.$emit('input', {
+        ...this.value,
+        [propName]: newValue,
+      });
+    },
     onSubmit() {
       this.showValidationStatus = true;
       if (this.$refs.form.checkValidity()) {
-        this.createOrUpdateProject();
+        this.$emit('submit');
       } else {
         this.$refs.form.reportValidity();
-      }
-    },
-    loadProjectData() {
-      this.errorMessages = [];
-      api.getProject(this.veranstaltungId).then((project) => {
-        this.veranstaltung = project;
-      }).catch((e) => this.errorMessages.push(e.toString()));
-    },
-    createOrUpdateProject() {
-      this.errorMessages = [];
-      if (this.isNewProject) {
-        api.createProject(this.veranstaltung).then(() => {
-          this.dismissCountDown = 5;
-        }).catch((errorMessages) => { this.errorMessages = errorMessages; });
-      } else {
-        api.updateProject(this.veranstaltung).then(() => {
-          this.dismissCountDown = 5;
-        }).catch((errorMessages) => { this.errorMessages = errorMessages; });
       }
     },
   },
--- frontend/src/views/VeranstaltungEditNew.vue
+++ frontend/src/views/VeranstaltungEditNew.vue
@@ -2,9 +2,29 @@
   <div>
     <NavigationMenu />
     <b-container>
-      <Veranstaltung
-        :veranstaltung-id="routeId"
+      <h1>
+        {{ titleText }}
+      </h1>
+      <ErrorAlert
+        v-if="showServerErrorAlert"
+        :heading-text="serverErrorHeadingText"
+        :errors="serverErrorMessages"
       />
+      <Veranstaltung
+        v-model="veranstaltung"
+        :submit-button-text="submitButtonText"
+        @submit="createOrUpdateProject"
+      />
+      <b-alert
+        class="fixed-bottom w-50 mx-auto"
+        :show="successAutomaticDismissCountDown"
+        dismissible
+        variant="success"
+        @dismissed="successAutomaticDismissCountDown=0"
+        @dismiss-count-down="successAutomaticDismissCountDown = $event"
+      >
+        {{ successText }}
+      </b-alert>
     </b-container>
   </div>
 </template>
@@ -12,13 +32,82 @@
 <script>
 import NavigationMenu from '@/components/NavBar.vue';
 import Veranstaltung from '@/components/VeranstaltungEditor.vue';
+import api from '@/modules/ferienpass-api';
+import ErrorAlert from '@/components/ErrorAlert.vue';
 
 export default {
   name: 'VeranstaltungEditNew',
-  components: { Veranstaltung, NavigationMenu },
+  components: { Veranstaltung, NavigationMenu, ErrorAlert },
+  data() {
+    return {
+      veranstaltung: {
+        /* TODO these have to be default values on the server model */
+        aktiv: true,
+        anmeldungen: [],
+        stornierteTeilnehmer: [],
+      },
+      serverErrorMessages: [],
+      successAutomaticDismissCountDown: 0,
+    };
+  },
   computed: {
-    routeId() {
+    veranstaltungId() {
       return parseInt(this.$route.query.id, 10);
+    },
+    isNewProject() {
+      return this.veranstaltungId <= 0;
+    },
+    titleText() {
+      if (this.isNewProject) {
+        return 'Veranstaltung anlegen';
+      }
+      return 'Veranstaltungsbearbeitung';
+    },
+    submitButtonText() {
+      if (this.isNewProject) {
+        return 'Anlegen';
+      }
+      return 'Speichern';
+    },
+    successText() {
+      if (this.isNewProject) {
+        return 'Veranstaltung erfolgreich angelegt.';
+      }
+      return 'Veranstaltung erfolgreich gespeichert.';
+    },
+    serverErrorHeadingText() {
+      if (this.isNewProject) {
+        return 'Anlegen nicht möglich. Bitte beheben Sie folgende Fehler:';
+      }
+      return 'Speichern nicht möglich. Bitte beheben Sie folgende Fehler:';
+    },
+    showServerErrorAlert() {
+      return this.serverErrorMessages.length > 0;
+    },
+  },
+  created() {
+    if (!this.isNewProject) {
+      this.loadProjectData();
+    }
+  },
+  methods: {
+    loadProjectData() {
+      this.serverErrorMessages = [];
+      api.getProject(this.veranstaltungId).then((project) => {
+        this.veranstaltung = project;
+      }).catch((e) => this.serverErrorMessages.push(e.toString()));
+    },
+    createOrUpdateProject() {
+      this.serverErrorMessages = [];
+      if (this.isNewProject) {
+        api.createProject(this.veranstaltung).then(() => {
+          this.successAutomaticDismissCountDown = 5;
+        }).catch((errorMessages) => { this.serverErrorMessages = errorMessages; });
+      } else {
+        api.updateProject(this.veranstaltung).then(() => {
+          this.successAutomaticDismissCountDown = 5;
+        }).catch((errorMessages) => { this.serverErrorMessages = errorMessages; });
+      }
     },
   },
 };
