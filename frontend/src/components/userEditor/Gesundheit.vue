c8af6ebe85c753b210d8769722e78ee7a1b8f9d8
Bertram Vogel <bertram.vogel@codecentric.de>
2020-03-06 01:32:02 +0100

    #91: tag group is now more similar to current implementation

--- frontend/src/components/userEditor/Gesundheit.vue
+++ frontend/src/components/userEditor/Gesundheit.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <h2>Allergien, Krankheiten, ...</h2>
+
+    <b-form-group
+      id="gesundheit-allergien2-group"
+      label-for="gesundheit-allergien2-value"
+      label="Bei meinem Kind muss auf folgende Allergie(n) geachtet werden:"
+    >
+      <b-form-tags
+        id="gesundheit-allergien2-tags"
+        input-id="gesundheit-allergien2-value"
+        tag-variant="primary"
+        placeholder="z.B. Heuschnupfen"
+        add-button-text="Hinzufügen"
+        duplicate-tag-text="Doppelter Eintrag"
+        :value="allergien"
+        @input="$emit('update:allergien', $event)"
+      />
+    </b-form-group>
+
     <b-form-group
       id="gesundheit-allergien-group"
       label-for="gesundheit-allergien-value"
@@ -10,13 +28,46 @@
         id="gesundheit-allergien-tags"
         input-id="gesundheit-allergien-value"
         tag-variant="primary"
-        tag-pills
+        no-outer-focus
         placeholder="z.B. Heuschnupfen"
         add-button-text="Hinzufügen"
         duplicate-tag-text="Doppelter Eintrag"
         :value="allergien"
         @input="$emit('update:allergien', $event)"
-      />
+      >
+        <template
+          v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag,
+                    addButtonText, placeholder }"
+        >
+          <div class="mb-2">
+            <b-form-tag
+              v-for="tag in tags"
+              :key="tag"
+              :title="tag"
+              :variant="tagVariant"
+              class="mr-1"
+              @remove="removeTag(tag)"
+            >
+              {{ tag }}
+            </b-form-tag>
+          </div>
+          <b-input-group>
+            <b-form-input
+              v-bind="inputAttrs"
+              :placeholder="placeholder"
+              v-on="inputHandlers"
+            />
+            <b-input-group-append>
+              <b-button
+                variant="primary"
+                @click="addTag()"
+              >
+                {{ addButtonText }}
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </template>
+      </b-form-tags>
     </b-form-group>
 
     <b-form-group
