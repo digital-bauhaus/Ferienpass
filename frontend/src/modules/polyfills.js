f2e1781e8a32a75cf668dd6f0ff63dbf6c44fa1c
Bertram Vogel <bertram.vogel@codecentric.de>
2020-03-12 23:07:54 +0100

    #107 trying another poly

--- frontend/src/modules/polyfills.js
+++ frontend/src/modules/polyfills.js
@@ -11,10 +11,47 @@ import 'regenerator-runtime/runtime';
 
 // Polyfill for reportValidity for IE
 // see: https://www.tjvantoll.com/2015/01/28/reportvalidity/
+// see: https://gist.github.com/nuxodin/73a2c2423cbbf6818c28ad803985d5c7
 if (!HTMLFormElement.prototype.reportValidity) {
-  HTMLFormElement.prototype.reportValidity = function reportValidity() {
-    console.log('reportValidity Handler');
-    return this.checkValidity() || this.submit();
+  HTMLFormElement.prototype.reportValidity = function reportValidityPoly() {
+    if (this.checkValidity()) return true;
+    const btn = document.createElement('button');
+    this.appendChild(btn);
+    btn.click();
+    this.removeChild(btn);
+    return false;
+  };
+}
+
+if (!HTMLInputElement.prototype.reportValidity) {
+  HTMLInputElement.prototype.reportValidity = function reportValidityPoly() {
+    if (this.checkValidity()) return true;
+    let tmpForm;
+    if (!this.form) {
+      tmpForm = document.createElement('form');
+      tmpForm.style.display = 'inline';
+      this.before(tmpForm);
+      tmpForm.append(this);
+    }
+    const siblings = Array.from(this.form.elements).filter(function doFilter(input) {
+      return input !== this && !!input.checkValidity && !input.disabled;
+    }, this);
+    siblings.forEach((input) => {
+      // eslint-disable-next-line no-param-reassign
+      input.disabled = true;
+    });
+    this.form.reportValidity();
+    siblings.forEach((input) => {
+      // eslint-disable-next-line no-param-reassign
+      input.disabled = false;
+    });
+    if (tmpForm) {
+      tmpForm.before(this);
+      tmpForm.remove();
+    }
+    this.focus();
+    this.selectionStart = 0;
+    return false;
   };
 }
 
