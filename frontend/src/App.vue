b5821a3b51455e3733ed7e08bfb601b35d717a9f
Bertram Vogel <bertram.vogel@codecentric.de>
2020-03-12 23:27:32 +0100

    #107 re-add routing fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,8 +5,29 @@
 </template>
 
 <script>
+
 export default {
   name: 'App',
+  created() {
+    this.applyIeRoutingFix();
+  },
+  methods: {
+    isInternetExplorer11() {
+      // https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
+      return !!window.MSInputMethodContext && !!document.documentMode;
+    },
+    applyIeRoutingFix() {
+      // https://github.com/vuejs/vue-router/issues/1849
+      if (this.isInternetExplorer11()) {
+        window.addEventListener('hashchange', () => {
+          const currentPath = window.location.hash.slice(1);
+          if (this.$route.path !== currentPath) {
+            this.$router.push(currentPath);
+          }
+        }, false);
+      }
+    },
+  },
 };
 </script>
 
