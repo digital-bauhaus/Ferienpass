4d3b8180eae8d712b75df8289454f4fb9e1542b5
Bertram Vogel <bertram.vogel@codecentric.de>
2020-03-13 08:49:42 +0100

    Hotfix for old incorrect column name

--- backend/src/main/java/de/bauhaus/digital/domain/Teilnehmer.java
+++ backend/src/main/java/de/bauhaus/digital/domain/Teilnehmer.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -105,6 +106,7 @@ public class Teilnehmer {
 
     private boolean essenVegetarier;
     private boolean essenLaktoseUnvertraeglichkeit;
+    @Column(name = 'essen_einer_unvertraeglichkeit') // TODO remove
     private boolean essenEierUnvertraeglichkeit;
     private String essenWeitereLimitierungen;
 
